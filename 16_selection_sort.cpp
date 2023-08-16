d45a2675bb4ab7191f1498d473c8c22cf6d44ef0
bassam165 <abssamrajbd65@gamil.com>
2023-08-17 02:09:03 +0600

    17-8-23

---
+++ 16_selection_sort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int selectionSort(int arr[],int n){
+    for(int i=0; i<n-1; i++){
+        int minIndex=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIndex])
+            minIndex=j;
+        }
+        swap(arr[minIndex],arr[i]);
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+}
+int main(){
+    int arr[5]={2,5,9,6,8};
+    selectionSort(arr,5);
+}
