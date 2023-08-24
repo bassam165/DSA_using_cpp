289ffd327f47aab3cdb6e43646c195e6c635f407
bassam165 <abssamrajbd65@gamil.com>
2023-08-24 09:44:58 +0600

    swap

--- swap2_array_number.cpp
+++ swap2_array_number.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
+
 void swapTwo(int arr[],int n){
     for(int i=0; i<=n; i+=2){
         if(i+1<n){
-            swap(arr[i],arr[i+1]);
+            swap(arr[i],arr[i+1]);//swap this
         }
     }
     for(int j=0; j<n; j++){
         cout<<arr[j];
     }
 }
+
 int main()
 {
     int arr[4]={2,5,6,8};
