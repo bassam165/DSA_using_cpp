7bcd5cef4232a4d5fcb3954f6f79aa8ce609cf7f
bassam165 <abssamrajbd65@gamil.com>
2023-08-25 20:12:30 +0600

    8-25-23

---
+++ 21_rotted_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void rotted(int arr[], int n, int k){
+    int temp[n];
+    for(int i=0; i<n; i++){
+        temp[(i+k)%n]=arr[i];
+    }
+    arr=temp;
+    for(int j=0; j<n; j++){
+        cout<<arr[j];
+    }
+}
+int main()
+{
+    int arr[4]={2,5,6,8};
+    rotted(arr,4,2);
+}
