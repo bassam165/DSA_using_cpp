d227dd35fd73c32d7d886576f923edd4dafbff07
bassam165 <abssamrajbd65@gamil.com>
2023-08-25 15:47:34 +0600

    25-8-23

---
+++ 20_move_zeros.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void move(int arr[], int n){
+    int i=0;
+    for(int j=0; j<n; j++){
+        if(arr[j] != 0){
+            swap(arr[j],arr[i]);
+            i++;
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+} 
+int main()
+{
+    int arr[5]={0,1,0,3,12};
+    move(arr, 5);
+}
