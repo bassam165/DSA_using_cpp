fe5ce4a12010b26b70b489fbfa56eda53203e4bc
Abdulla Al Bassam <abassamrajbd65@gmail.com>
2024-06-28 10:00:44 +0600

    linked list

---
+++ Linked_list.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+    
+    //constructor
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int d){
+    //new node create
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp-> data<< endl;
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node* node1 = new Node(10);
+    cout<< node1 -> data << endl;
+    cout<< node1 -> next << endl;
+    
+    //node pinted
+    Node* head = node1;
+    print(head);
+    insertAtHead(head, 12);
+    cout<<head<<endl;
+    print(head);
+    return 0;
+}
